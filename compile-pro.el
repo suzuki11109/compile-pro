49762e67ac2be8989b5ea3f20af2911d0b4384ca
Aki Suzuki <suzuki11109@gmail.com>
2026-01-18 11:54:12 +0700

    bump version

--- compile-pro.el
+++ compile-pro.el
@@ -1,7 +1,7 @@
 ;;; compile-pro.el --- Per-project compile utitlities for Emacs -*- lexical-binding: t; -*-
 
 ;; Author: Aki Suzuki <suzuki11109@gmail.com>
-;; Version: 0.1.1
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: tools, compile, project
 ;; URL: https://github.com/suzuki11109/compile-pro
